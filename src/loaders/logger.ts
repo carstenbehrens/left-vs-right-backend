46dcd12aba31f596e2bfb9cb3572aae07f6d39bd
carsten.behrens1@googlemail.com <carsten.behrens1@googlemail.com>
2020-12-29 21:09:42 +0100

    Improve logger implementation

--- src/loaders/logger.ts
+++ src/loaders/logger.ts
@@ -3,16 +3,16 @@ import { ILogger } from '../interfaces/ILogger';
 @Service()
 class Logger implements ILogger {
   public debug(msg: string | Error, ...optionalParams: any[]): void {
-    console.debug(msg, ...optionalParams);
+    console.debug(`Debug: ${msg}`, ...optionalParams);
   }
   public info(msg: string | Error, ...optionalParams: any[]): void {
-    console.info(msg, ...optionalParams);
+    console.info(`Info: ${msg}`, ...optionalParams);
   }
   public warn(msg: string | Error, ...optionalParams: any[]): void {
-    console.warn(msg, ...optionalParams);
+    console.warn(`Warn: ${msg}`, ...optionalParams);
   }
   public error(msg: string | Error, ...optionalParams: any[]): void {
-    console.error(msg, ...optionalParams);
+    console.error(`Error: ${msg}`, ...optionalParams);
   }
 }
 
