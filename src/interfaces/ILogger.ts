ff0714a46f205fc0be86e236d13287da20f19374
Carsten Behrens <carsten.behrens1@googlemail.com>
2021-01-20 20:28:52 +0100

    Fix logger interface

--- src/interfaces/ILogger.ts
+++ src/interfaces/ILogger.ts
@@ -1,6 +1,6 @@
 export interface ILogger {
-  debug(message: string, ...optionalParams: any[]): void;
-  warn(message: string, ...optionalParams: any[]): void;
-  error(message: string, ...optionalParams: any[]): void;
-  info(message: string, ...optionalParams: any[]): void;
+  debug(message: string | Error, ...optionalParams: any[]): void;
+  warn(message: string | Error, ...optionalParams: any[]): void;
+  error(message: string | Error, ...optionalParams: any[]): void;
+  info(message: string | Error, ...optionalParams: any[]): void;
 }
